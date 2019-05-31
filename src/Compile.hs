e248e801d51f94696a1b44ddd3543ccb19903c23
Johan Johansson <jo@jo.zone>
2019-06-01 01:50:00 +0200

    Beyond out.o, also output out.ll, out.bc for debugging purposes

--- src/Compile.hs
+++ src/Compile.hs
@@ -4,11 +4,14 @@ import qualified LLVM.AST
 import LLVM.Context
 import LLVM.Module
 import LLVM.Target
-import Data.Function.Slip
 
 -- TODO: Verify w LLVM.Analysis.verify :: Module -> IO ()
 -- TODO: CodeGenOpt level
 compile :: LLVM.AST.Module -> IO ()
-compile mod = withContext $ (slipr withModuleFromAST)
-    mod
-    (withHostTargetMachine . slipr writeObjectToFile (File "out.o"))
+compile mod = withContext $ \c -> withModuleFromAST c mod compileModule
+
+compileModule :: Module -> IO ()
+compileModule m = withHostTargetMachine $ \t -> do
+    writeLLVMAssemblyToFile (File "out.ll") m
+    writeBitcodeToFile (File "out.bc") m
+    writeObjectToFile t (File "out.o") m
