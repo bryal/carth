ce1f6d3751d523e95840e14cf0e56c1d19d77ac8
Johan Johansson <jo@jo.zone>
2019-06-02 03:39:55 +0200

    Generate PIC (Position Independent Code) for better compatibility

--- src/Compile.hs
+++ src/Compile.hs
@@ -4,6 +4,9 @@ import qualified LLVM.AST
 import LLVM.Context
 import LLVM.Module
 import LLVM.Target
+import qualified LLVM.Relocation as Reloc
+import qualified LLVM.CodeModel as CodeModel
+import qualified LLVM.CodeGenOpt as CodeGenOpt
 
 -- TODO: Verify w LLVM.Analysis.verify :: Module -> IO ()
 -- TODO: CodeGenOpt level
@@ -11,7 +14,25 @@ compile :: LLVM.AST.Module -> IO ()
 compile mod = withContext $ \c -> withModuleFromAST c mod compileModule
 
 compileModule :: Module -> IO ()
-compileModule m = withHostTargetMachine $ \t -> do
     writeLLVMAssemblyToFile (File "out.ll") m
     writeBitcodeToFile (File "out.bc") m
     writeObjectToFile t (File "out.o") m
+compileModule m = withHostTargetMachinePIC $ \t -> do
+
+withHostTargetMachinePIC :: (TargetMachine -> IO a) -> IO a
+withHostTargetMachinePIC f = do
+    initializeAllTargets
+    trip <- getDefaultTargetTriple
+    (targ, _) <- lookupTarget Nothing trip
+    cpu <- getHostCPUName
+    features <- getHostCPUFeatures
+    withTargetOptions $ \opts -> withTargetMachine
+        targ
+        trip
+        cpu
+        features
+        opts
+        Reloc.PIC
+        CodeModel.Default
+        CodeGenOpt.Aggressive
+        f
