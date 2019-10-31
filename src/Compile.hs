a590552424381009d474242fd068e6e9b340f039
JoJo <jo@jo.zone>
2019-10-31 20:31:34 +0100

    Add workaround for `writeLLVMAssemblyToFile` not clearing old content
    It was so annoying having the `out.ll` files frequently being
    un-compilable due to incomplete overwrites after a reducting change.

--- src/Compile.hs
+++ src/Compile.hs
@@ -40,7 +40,7 @@ compileModule cfg m = withHostTargetMachinePIC $ \t -> do
         llfile = replaceExtension binfile "ll"
         bcfile = replaceExtension binfile "bc"
         ofile = replaceExtension binfile "o"
-    writeLLVMAssemblyToFile (File llfile) m
+    writeLLVMAssemblyToFile' llfile m
     writeBitcodeToFile (File bcfile) m
     writeObjectToFile t (File ofile) m
     callProcess
@@ -52,6 +52,19 @@ compileModule cfg m = withHostTargetMachinePIC $ \t -> do
         , "-ldl"
         , "-lpthread"
         ]
+
+-- | `writeLLVMAssemblyToFile` doesn't clear file contents before writing, so this
+--   is a workaround.
+--
+--   If the file was previously 100 lines of data, and the new LLVM-assembly is
+--   70 lines, the first 70 lines will be overwritten, but the remaining 30 will
+--   be the same as in the old file, which will cause errors if we try to
+--   compile it manually. So we have to clear file contents first manually if we
+--   want these dumps to be useful for debugging.
+writeLLVMAssemblyToFile' :: FilePath -> Module -> IO ()
+writeLLVMAssemblyToFile' f m = do
+    writeFile f ""
+    writeLLVMAssemblyToFile (File f) m
 
 withHostTargetMachinePIC :: (TargetMachine -> IO a) -> IO a
 withHostTargetMachinePIC f = do
