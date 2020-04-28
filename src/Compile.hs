2e335e3be1a7bf254522fb83811a3a1131318117
JoJo <jo@jo.zone>
2020-04-29 01:10:03 +0200

    Add (broken) OrcJIT impl with `orcJitModule`, `runOrc`
    OrcJIT provides more control than MCJIT and is newer, so I would
    prefer using that. However, even after trying for hours, I just can't
    get it working. I can get it working for simple cases, where we don't
    refer to any symbols outside of the LLVM module. As soon as we try to
    include something like carth_foreign_core, things break down. I've
    added a FIXME with some more explanation of the problem.

--- src/Compile.hs
+++ src/Compile.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE ForeignFunctionInterface, OverloadedStrings, LambdaCase #-}
 
-module Compile (compile, run) where
+module Compile (compile, run, runOrc) where
 
 import LLVM.Context
 import LLVM.Module
 import LLVM.Target
 import LLVM.Analysis
+import LLVM.OrcJIT
+import LLVM.OrcJIT.CompileLayer
 import LLVM.ExecutionEngine
 import qualified LLVM.AST as LLAST
 import qualified LLVM.Relocation as Reloc
@@ -17,6 +19,8 @@ import System.Process
 import System.Exit
 import Data.Int
 import Data.Functor
+import Data.IORef
+import qualified Data.Map as Map
 import Foreign.Ptr
 import Prelude hiding (mod)
 
@@ -30,6 +34,9 @@ compile = handleProgram (const compileModule) cDebug
 
 run :: FilePath -> RunConfig -> Monomorphic.Program -> IO ()
 run = handleProgram (\ctx _ _ -> mcJitModule ctx) rDebug
+
+runOrc :: FilePath -> RunConfig -> Monomorphic.Program -> IO ()
+runOrc = handleProgram (\_ tm _ -> orcJitModule tm) rDebug
 
 -- TODO: CodeGenOpt level
 handleProgram
@@ -78,6 +85,52 @@ mcJitModule ctx mod = do
             getFunction execMod (LLAST.Name "main") >>= \case
                 Just mainAddr -> mkMain (castFunPtr mainAddr) $> ()
                 Nothing -> putStrLn "Error getting main" >> exitFailure
+
+-- FIXME: Works if there are no external symbols declared, i.e. if it's a
+--        carth-only program, so to speak. As soon as we need symbols from
+--        carth_foreign_core or so, it breaks down.
+--
+--        If we load libcarth_foreign_core.{so/a} with extra-libraries in
+--        package.yaml, there's no difference. If we load the .so with
+--        loadLibraryPermanently, getSymbolAddressInProcess can find
+--        e.g. add-int, but symbol resolution still fails at some point, and the
+--        last printed line from `resolver` says `add-int`, so idk what's going
+--        on.
+orcJitModule :: TargetMachine -> Module -> IO ()
+orcJitModule tm mod = do
+    putStrLn
+        "   Running with OrcJIT (Broken for me. If working for you, please tell me!!)"
+    -- loadLibraryPermanently Nothing
+    --     >>= \b -> putStrLn $ "   load self: " ++ show (not b)
+    -- loadLibraryPermanently
+    --         (Just "../foreign-core/target/release/libcarth_foreign_core.so")
+    --     >>= \b -> putStrLn $ "   load foreign core: " ++ show (not b)
+    resolvers <- newIORef Map.empty
+    let linkingResolver key = fmap (Map.! key) (readIORef resolvers)
+    session <- createExecutionSession
+    linkLay <- newObjectLinkingLayer session linkingResolver
+    compLay <- newIRCompileLayer linkLay tm
+    let resolver symbol = do
+            -- putStrLn ("Resolving symbol " ++ show symbol)
+            findSymbol compLay symbol False
+    withSymbolResolver session (SymbolResolver resolver) $ \resolverPtr ->
+        withModuleKey session $ \modKey -> do
+            modifyIORef' resolvers (Map.insert modKey resolverPtr)
+            withModule compLay modKey mod $ do
+                -- (putStrLn . ("add-int: " ++) . show)
+                --     =<< getSymbolAddressInProcess
+                --     =<< mangleSymbol compLay "add-int"
+                mainSymbol <- mangleSymbol compLay "main"
+                resolver mainSymbol >>= \case
+                    Left err -> do
+                        putStrLn "Error during JIT symbol resolution"
+                        print err
+                        exitFailure
+                    Right (JITSymbol mainAddr _) ->
+                        mkMain (castPtrToFunPtr (wordPtrToPtr mainAddr)) $> ()
+    disposeCompileLayer compLay
+    disposeLinkingLayer linkLay
+    disposeExecutionSession session
 
 -- | `writeLLVMAssemblyToFile` doesn't clear file contents before writing,
 --   so this is a workaround.
