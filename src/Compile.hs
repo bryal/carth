8301cf54fa2fd4eb8a9ad7525222a4b5102d36fb
JoJo <jo@jo.zone>
2020-04-29 17:43:31 +0200

    Got OrcJIT to work! Look in process address space in resolver.
    Previously, I was trying to resolve everything with
    CompileLayer.findSymbol, however, this function only looks for symbols
    in the compile-layer (i.e. LLVM module). LingingLayer.findSymbol
    also includes manually added .o files, but this is also not enough. To
    get parity with the MCJIT version, we have to look in the address
    space of the running process. This has to be done with
    `getSymbolAddressInProcess` (after `loadLibraryPermanently Nothing`).

--- src/Compile.hs
+++ src/Compile.hs
@@ -1,18 +1,20 @@
-{-# LANGUAGE ForeignFunctionInterface, OverloadedStrings, LambdaCase #-}
+{-# LANGUAGE ForeignFunctionInterface, OverloadedStrings, LambdaCase
+           , OverloadedStrings #-}
 
-module Compile (compile, run, runOrc) where
+module Compile (compile, run) where
 
 import LLVM.Context
 import LLVM.Module
 import LLVM.Target
 import LLVM.Analysis
 import LLVM.OrcJIT
-import LLVM.OrcJIT.CompileLayer
-import LLVM.ExecutionEngine
-import qualified LLVM.AST as LLAST
+import LLVM.OrcJIT.LinkingLayer as LL
+import LLVM.Linking
+import LLVM.Internal.OrcJIT (MangledSymbol(..))
 import qualified LLVM.Relocation as Reloc
 import qualified LLVM.CodeModel as CodeModel
 import qualified LLVM.CodeGenOpt as CodeGenOpt
+import Data.ByteString.Short (toShort)
 import Control.Monad
 import System.FilePath
 import System.Process
@@ -20,6 +22,7 @@ import System.Exit
 import Data.Int
 import Data.Functor
 import Data.IORef
+import Data.String
 import qualified Data.Map as Map
 import Foreign.Ptr
 import Prelude hiding (mod)
@@ -30,17 +33,14 @@ import Codegen
 
 
 compile :: FilePath -> CompileConfig -> Monomorphic.Program -> IO ()
-compile = handleProgram (const compileModule) cDebug
+compile = handleProgram compileModule cDebug
 
 run :: FilePath -> RunConfig -> Monomorphic.Program -> IO ()
-run = handleProgram (\ctx _ _ -> mcJitModule ctx) rDebug
-
-runOrc :: FilePath -> RunConfig -> Monomorphic.Program -> IO ()
-runOrc = handleProgram (\_ tm _ -> orcJitModule tm) rDebug
+run = handleProgram (const orcJitModule) rDebug
 
 -- TODO: CodeGenOpt level
 handleProgram
-    :: (Context -> TargetMachine -> config -> Module -> IO ())
+    :: (config -> TargetMachine -> Module -> IO ())
     -> (config -> Bool)
     -> FilePath
     -> config
@@ -56,10 +56,10 @@ handleProgram f debug file cfg pgm = withContext $ \ctx ->
             when (debug cfg) $ writeLLVMAssemblyToFile' ".dbg.ll" mod
             putStrLn ("   Verifying LLVM")
             verify mod
-            f ctx tm cfg mod
+            f cfg tm mod
 
-compileModule :: TargetMachine -> CompileConfig -> Module -> IO ()
-compileModule tm cfg mod = do
+compileModule :: CompileConfig -> TargetMachine -> Module -> IO ()
+compileModule cfg tm mod = do
     let exefile = cOutfile cfg
         ofile = replaceExtension exefile "o"
     writeObjectToFile tm (File ofile) mod
@@ -77,15 +77,6 @@ compileModule tm cfg mod = do
 foreign import ccall "dynamic"
   mkMain :: FunPtr (IO Int32) -> IO Int32
 
-mcJitModule :: Context -> Module -> IO ()
-mcJitModule ctx mod = do
-    putStrLn "   Running with MCJIT"
-    withMCJIT ctx Nothing Nothing Nothing Nothing $ \engine ->
-        withModuleInEngine engine mod $ \execMod ->
-            getFunction execMod (LLAST.Name "main") >>= \case
-                Just mainAddr -> mkMain (castFunPtr mainAddr) $> ()
-                Nothing -> putStrLn "Error getting main" >> exitFailure
-
 -- FIXME: Works if there are no external symbols declared, i.e. if it's a
 --        carth-only program, so to speak. As soon as we need symbols from
 --        carth_foreign_core or so, it breaks down.
@@ -98,32 +89,21 @@ mcJitModule ctx mod = do
 --        on.
 orcJitModule :: TargetMachine -> Module -> IO ()
 orcJitModule tm mod = do
-    putStrLn
-        "   Running with OrcJIT (Broken for me. If working for you, please tell me!!)"
-    -- loadLibraryPermanently Nothing
-    --     >>= \b -> putStrLn $ "   load self: " ++ show (not b)
-    -- loadLibraryPermanently
-    --         (Just "../foreign-core/target/release/libcarth_foreign_core.so")
-    --     >>= \b -> putStrLn $ "   load foreign core: " ++ show (not b)
+    putStrLn "   Running with OrcJIT"
+    putStrLn "   Loading symbols of running process"
+    _ <- loadLibraryPermanently Nothing
     resolvers <- newIORef Map.empty
     let linkingResolver key = fmap (Map.! key) (readIORef resolvers)
     session <- createExecutionSession
     linkLay <- newObjectLinkingLayer session linkingResolver
     compLay <- newIRCompileLayer linkLay tm
-    let resolver symbol = do
-            -- putStrLn ("Resolving symbol " ++ show symbol)
-            findSymbol compLay symbol False
-    withSymbolResolver session (SymbolResolver resolver) $ \resolverPtr ->
-        withModuleKey session $ \modKey -> do
+    withSymbolResolver session (SymbolResolver (resolver linkLay))
+        $ \resolverPtr -> withModuleKey session $ \modKey -> do
             modifyIORef' resolvers (Map.insert modKey resolverPtr)
             withModule compLay modKey mod $ do
-                -- (putStrLn . ("add-int: " ++) . show)
-                --     =<< getSymbolAddressInProcess
-                --     =<< mangleSymbol compLay "add-int"
-                mainSymbol <- mangleSymbol compLay "main"
-                resolver mainSymbol >>= \case
+                mangleSymbol compLay "main" >>= resolver linkLay >>= \case
                     Left err -> do
-                        putStrLn "Error during JIT symbol resolution"
+                        putStrLn "   Error during JIT symbol resolution"
                         print err
                         exitFailure
                     Right (JITSymbol mainAddr _) ->
@@ -131,6 +111,57 @@ orcJitModule tm mod = do
     disposeCompileLayer compLay
     disposeLinkingLayer linkLay
     disposeExecutionSession session
+
+-- Following are some useful things to know regarding symbol resolution when it
+-- comes to JIT, LLVM, and OrcJIT. I'm not sure about all of this, so take it
+-- with a grain of salt.
+--
+-- - `CompileLayer.findSymbol`: Only looks in the compile-layer, which includes
+--   our compiled LLVM modules, but not linked object code, or linked shared
+--   libraries.
+--
+-- - `LinkingLayer.findSymbol`: Looks in the linking-layer, a superset of the
+--   compile-layer that includes all object code added to the layer with
+--   `addObjectFile`. We might be able to compile foreign-core to a .o file and
+--   add it via this mechanism.
+--
+-- - `Linking.getSymbolAddressInProcess`: Looks in the address-space of the
+--   running process. If we link with libcarth_foreign_core.so when building
+--   carth, we can see the symbols with this. Cannot see `main` in the
+--   compile-layer, which I guess makes sense -- why would `withModule` add it's
+--   JITted symbols to the address-space of the carth process. Requires that the
+--   symbols of the current process are made available first with
+--   `Linking.loadLibraryPermanently Nothing`.
+resolver
+    :: ObjectLinkingLayer
+    -> MangledSymbol
+    -> IO (Either JITSymbolError JITSymbol)
+resolver linkLay (symb@(MangledSymbol bsymb)) = do
+    let ssymb = show bsymb
+    putStrLn ("   Resolving " ++ ssymb ++ " in linking layer")
+    LL.findSymbol linkLay (toShort bsymb) False >>= \case
+        Right jitsym -> do
+            putStrLn ("   Resolved " ++ ssymb)
+            pure (Right jitsym)
+        Left _ -> do
+            putStrLn $ "   Error resolving " ++ ssymb ++ " in linking layer"
+            putStrLn "   Resolving in running process"
+            addr <- getSymbolAddressInProcess symb
+            if addr == 0
+                then do
+                    let e =
+                            "Error resolving " ++ ssymb ++ " in running process"
+                    putStrLn ("   " ++ e)
+                    pure (Left (JITSymbolError (fromString e)))
+                else do
+                    putStrLn ("   Resolved " ++ ssymb)
+                    let flags = JITSymbolFlags
+                            { jitSymbolWeak = False
+                            , jitSymbolCommon = False
+                            , jitSymbolAbsolute = False
+                            , jitSymbolExported = True
+                            }
+                    pure (Right (JITSymbol addr flags))
 
 -- | `writeLLVMAssemblyToFile` doesn't clear file contents before writing,
 --   so this is a workaround.
