400ecae27dd419fc1688459b17c67a0e473d7f87
JoJo <jo@jo.zone>
2020-10-01 22:29:29 +0200

    Prevent optimizer from making main call a "tail" call
    Will break things if we change the sig of main to something like IO.

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -202,7 +202,7 @@ genMain = do
         emitDo' =<< callBuiltin "install_stackoverflow_handler" []
         emitDo (callIntern Nothing init_ [(null' typeGenericPtr, []), (litUnit, [])])
         f <- lookupVar (TypedVar "main" mainType)
-        _ <- app Nothing f (VLocal litUnit)
+        _ <- app (Just NoTail) f (VLocal litRealWorld)
         commitFinalFuncBlock (ret (litI32 0))
     pure (GlobalDefinition (externFunc (mkName "main") [] i32 basicBlocks []))
 
