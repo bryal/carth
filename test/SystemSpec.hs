d279c0b75b25b3ba19ed7d46d4269c65df77f080
JoJo <jo@jo.zone>
2022-08-02 01:10:37 +0200

    Test that programs compile with the C backend

--- test/SystemSpec.hs
+++ test/SystemSpec.hs
@@ -17,7 +17,9 @@ import Front.Macro
 import Front.Parse
 import qualified Front.Parsed as Parsed
 import Front.Check
-import Back.CompileLLVM
+import Back.CompileLLVM as LLVM
+import Back.CompileC as C
+import Back.Link
 import Front.Monomorphize
 import Back.Lower
 import qualified Back.Low as Low
@@ -33,6 +35,11 @@ spec = do
                 (unlines . map (drop 3) . takeWhile (isPrefixOf ";; ") . lines)
                 (readFile (d </> f))
             it (dropExtension f) $ shouldReturn (run' (d </> f)) expectedOut
+    describe "Good programs compile with C backend" $ do
+        let d = "test/tests/good"
+        fs <- runIO $ listDirectory d <&> filter isSourceFile
+        forM_ fs $ \f -> do
+            it (dropExtension f) $ shouldReturn (compile_C (d </> f)) True
     describe "Bad programs don't typecheck" $ do
         let d = "test/tests/bad"
         fs <- runIO $ listDirectory d <&> filter isSourceFile
@@ -42,12 +49,12 @@ spec = do
             it (dropExtension f) $ shouldSatisfy (fmap typecheck result) $ \case
                 Just (Left e) -> head (words (show e)) == expectedErr
                 _ -> False
-    describe "Examples compile & run correctly" $ do
+    describe "Examples compile correctly" $ do
         let d = "examples"
         fs <- runIO $ listDirectory d <&> filter isSourceFile
         forM_ fs $ \f -> do
             it (dropExtension f) $ shouldReturn (compile' (d </> f)) True
-    describe "Benchmarks compile & run correctly" $ do
+    describe "Benchmarks compile correctly" $ do
         let d = "test/bench"
         fs <- runIO $ listDirectory d <&> filter isSourceFile
         forM_ fs $ \f -> do
@@ -64,14 +71,21 @@ run' f =
             Just ast -> capture_ (run f cfg ast)
 
 compileLow :: FilePath -> Low.Program -> IO Bool
-compileLow f pgm = compile f ((defaultCompileConfig f) { cDebug = True }) pgm $> True
+compileLow f pgm = LLVM.compile f ((defaultCompileConfig f) { cDebug = True }) pgm $> True
 
 compile' :: FilePath -> IO Bool
 compile' f =
     let cfg = defaultCompileConfig f
     in  frontend f >>= \case
             Nothing -> pure False
-            Just ast -> compile f cfg ast $> True
+            Just ast -> LLVM.compile f cfg ast *> link cfg $> True
+
+compile_C :: FilePath -> IO Bool
+compile_C f =
+    let cfg = (defaultCompileConfig f) { cBackend = BendC }
+    in  frontend f >>= \case
+            Nothing -> pure False
+            Just ast -> C.compile cfg ast *> link cfg $> True
 
 frontend :: FilePath -> IO (Maybe Low.Program)
 frontend f = lexAndParse f <&> \case
